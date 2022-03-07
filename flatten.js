fa5ef8847b9bfcc1666f2567dcf577a4e2bbe31f
Berry Davenport <bdavenport@collabralink.com>
2022-03-07 17:06:14 -0500

    add timers

--- flatten.js
+++ flatten.js
@@ -1,5 +1,6 @@
 const htmlPdf = require('html-pdf-chrome');
-
+const begin = Date.now();
+console.log('Begin');
 const options = {
     port: 9222,
     completionTrigger: new htmlPdf.CompletionTrigger.Timer(50000),
@@ -7,4 +8,12 @@ const options = {
 };
 
 const url = 'http://localhost:8888/web/viewer.html?file=/xfa/xfa_invoice_example.pdf';
-let pdf = htmlPdf.create(url, options).then((pdf) => pdf.toFile('./output/output.pdf'));
+console.log("Requesting conversion");
+let pdf = htmlPdf.create(url, options).then((pdf) => {
+  const created = Date.now();
+  console.log(`Pdf is converted, writing output: ${created-begin}`);
+  const result = pdf.toFile('./output/output.pdf');
+  const end = Date.now();
+  console.log(`Output is written: ${end-created}, ${end-begin}`);
+  return result;
+});
